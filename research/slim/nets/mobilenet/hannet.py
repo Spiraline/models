c7382d48c2abd23b42ffc772adfc41197a7ce584
spiraline <spiraline@naver.com>
2020-01-07 17:48:31 +0900

    Custom operation in hannet

--- research/slim/nets/mobilenet/hannet.py
+++ research/slim/nets/mobilenet/hannet.py
@@ -129,147 +129,38 @@ DEFAULTS = {
     },
 }
 
-# Compatible checkpoint: http://mldash/5511169891790690458#scalars
-V3_LARGE = dict(
+
+V3_LARGE_ALLOW_HARD_SWISH = dict(
     defaults=dict(DEFAULTS),
     spec=([
         # stage 1
         op(slim.conv2d, stride=2, num_outputs=16, kernel_size=(3, 3),
            activation_fn=hard_swish),
-        mbv3_op(ef=1, n=16, k=3),
-        mbv3_op(ef=4, n=24, k=3, s=2),
-        mbv3_op(ef=3, n=24, k=3, s=1),
-        mbv3_op_se(ef=3, n=40, k=5, s=2),
-        mbv3_op_se(ef=3, n=40, k=5, s=1),
-        mbv3_op_se(ef=3, n=40, k=5, s=1),
-        mbv3_op(ef=6, n=80, k=3, s=2, act=hard_swish),
-        mbv3_op(ef=2.5, n=80, k=3, s=1, act=hard_swish),
-        mbv3_op(ef=184/80., n=80, k=3, s=1, act=hard_swish),
-        mbv3_op(ef=184/80., n=80, k=3, s=1, act=hard_swish),
-        mbv3_op_se(ef=6, n=112, k=3, s=1, act=hard_swish),
-        mbv3_op_se(ef=6, n=112, k=3, s=1, act=hard_swish),
-        mbv3_op_se(ef=6, n=160, k=5, s=2, act=hard_swish),
-        mbv3_op_se(ef=6, n=160, k=5, s=1, act=hard_swish),
-        mbv3_op_se(ef=6, n=160, k=5, s=1, act=hard_swish),
-        op(slim.conv2d, stride=1, kernel_size=[1, 1], num_outputs=960,
-           activation_fn=hard_swish),
-        op(reduce_to_1x1, default_size=7, stride=1, padding='VALID'),
-        op(slim.conv2d, stride=1, kernel_size=[1, 1], num_outputs=1280,
-           normalizer_fn=None, activation_fn=hard_swish)
-    ]))
-
-# 72.2% accuracy.
-V3_LARGE_MINIMALISTIC = dict(
-    defaults=dict(DEFAULTS),
-    spec=([
-        # stage 1
-        op(slim.conv2d, stride=2, num_outputs=16, kernel_size=(3, 3)),
         mbv3_op(ef=1, n=16, k=3),
         mbv3_op(ef=4, n=24, k=3, s=2),
         mbv3_op(ef=3, n=24, k=3, s=1),
         mbv3_op(ef=3, n=40, k=3, s=2),
         mbv3_op(ef=3, n=40, k=3, s=1),
         mbv3_op(ef=3, n=40, k=3, s=1),
-        mbv3_op(ef=6, n=80, k=3, s=2),
-        mbv3_op(ef=2.5, n=80, k=3, s=1),
-        mbv3_op(ef=184 / 80., n=80, k=3, s=1),
-        mbv3_op(ef=184 / 80., n=80, k=3, s=1),
-        mbv3_op(ef=6, n=112, k=3, s=1),
-        mbv3_op(ef=6, n=112, k=3, s=1),
-        mbv3_op(ef=6, n=160, k=3, s=2),
-        mbv3_op(ef=6, n=160, k=3, s=1),
-        mbv3_op(ef=6, n=160, k=3, s=1),
-        op(slim.conv2d, stride=1, kernel_size=[1, 1], num_outputs=960),
+        mbv3_op(ef=6, n=80, k=3, s=2, act=hard_swish),
+        mbv3_op(ef=2.5, n=80, k=3, s=1, act=hard_swish),
+        mbv3_op(ef=184 / 80., n=80, k=3, s=1, act=hard_swish),
+        mbv3_op(ef=184 / 80., n=80, k=3, s=1, act=hard_swish),
+        mbv3_op(ef=6, n=112, k=3, s=1, act=hard_swish),
+        mbv3_op(ef=6, n=112, k=3, s=1, act=hard_swish),
+        mbv3_op(ef=6, n=160, k=3, s=2, act=hard_swish),
+        mbv3_op(ef=6, n=160, k=3, s=1, act=hard_swish),
+        mbv3_op(ef=6, n=160, k=3, s=1, act=hard_swish),
+        op(slim.conv2d, stride=1, kernel_size=[1, 1], num_outputs=960,
+           activation_fn=hard_swish),
         op(reduce_to_1x1, default_size=7, stride=1, padding='VALID'),
         op(slim.conv2d,
            stride=1,
            kernel_size=[1, 1],
            num_outputs=1280,
-           normalizer_fn=None)
+           normalizer_fn=None,
+           activation_fn=hard_swish)
     ]))
-
-# Compatible run: http://mldash/2023283040014348118#scalars
-V3_SMALL = dict(
-    defaults=dict(DEFAULTS),
-    spec=([
-        # stage 1
-        op(slim.conv2d, stride=2, num_outputs=16, kernel_size=(3, 3),
-           activation_fn=hard_swish),
-        mbv3_op_se(ef=1, n=16, k=3, s=2),
-        mbv3_op(ef=72./16, n=24, k=3, s=2),
-        mbv3_op(ef=(88./24), n=24, k=3, s=1),
-        mbv3_op_se(ef=4, n=40, k=5, s=2, act=hard_swish),
-        mbv3_op_se(ef=6, n=40, k=5, s=1, act=hard_swish),
-        mbv3_op_se(ef=6, n=40, k=5, s=1, act=hard_swish),
-        mbv3_op_se(ef=3, n=48, k=5, s=1, act=hard_swish),
-        mbv3_op_se(ef=3, n=48, k=5, s=1, act=hard_swish),
-        mbv3_op_se(ef=6, n=96, k=5, s=2, act=hard_swish),
-        mbv3_op_se(ef=6, n=96, k=5, s=1, act=hard_swish),
-        mbv3_op_se(ef=6, n=96, k=5, s=1, act=hard_swish),
-        op(slim.conv2d, stride=1, kernel_size=[1, 1], num_outputs=576,
-           activation_fn=hard_swish),
-        op(reduce_to_1x1, default_size=7, stride=1, padding='VALID'),
-        op(slim.conv2d, stride=1, kernel_size=[1, 1], num_outputs=1024,
-           normalizer_fn=None, activation_fn=hard_swish)
-    ]))
-
-# 62% accuracy.
-V3_SMALL_MINIMALISTIC = dict(
-    defaults=dict(DEFAULTS),
-    spec=([
-        # stage 1
-        op(slim.conv2d, stride=2, num_outputs=16, kernel_size=(3, 3)),
-        mbv3_op(ef=1, n=16, k=3, s=2),
-        mbv3_op(ef=72. / 16, n=24, k=3, s=2),
-        mbv3_op(ef=(88. / 24), n=24, k=3, s=1),
-        mbv3_op(ef=4, n=40, k=3, s=2),
-        mbv3_op(ef=6, n=40, k=3, s=1),
-        mbv3_op(ef=6, n=40, k=3, s=1),
-        mbv3_op(ef=3, n=48, k=3, s=1),
-        mbv3_op(ef=3, n=48, k=3, s=1),
-        mbv3_op(ef=6, n=96, k=3, s=2),
-        mbv3_op(ef=6, n=96, k=3, s=1),
-        mbv3_op(ef=6, n=96, k=3, s=1),
-        op(slim.conv2d, stride=1, kernel_size=[1, 1], num_outputs=576),
-        op(reduce_to_1x1, default_size=7, stride=1, padding='VALID'),
-        op(slim.conv2d,
-           stride=1,
-           kernel_size=[1, 1],
-           num_outputs=1024,
-           normalizer_fn=None)
-    ]))
-
-
-# EdgeTPU friendly variant of MobilenetV3 that uses fused convolutions
-# instead of depthwise in the early layers.
-V3_EDGETPU = dict(
-    defaults=dict(DEFAULTS),
-    spec=[
-        op(slim.conv2d, stride=2, num_outputs=32, kernel_size=(3, 3)),
-        mbv3_fused(k=3, s=1, ef=1, n=16),
-        mbv3_fused(k=3, s=2, ef=8, n=32),
-        mbv3_fused(k=3, s=1, ef=4, n=32),
-        mbv3_fused(k=3, s=1, ef=4, n=32),
-        mbv3_fused(k=3, s=1, ef=4, n=32),
-        mbv3_fused(k=3, s=2, ef=8, n=48),
-        mbv3_fused(k=3, s=1, ef=4, n=48),
-        mbv3_fused(k=3, s=1, ef=4, n=48),
-        mbv3_fused(k=3, s=1, ef=4, n=48),
-        mbv3_op(k=3, s=2, ef=8, n=96),
-        mbv3_op(k=3, s=1, ef=4, n=96),
-        mbv3_op(k=3, s=1, ef=4, n=96),
-        mbv3_op(k=3, s=1, ef=4, n=96),
-        mbv3_op(k=3, s=1, ef=8, n=96, residual=False),
-        mbv3_op(k=3, s=1, ef=4, n=96),
-        mbv3_op(k=3, s=1, ef=4, n=96),
-        mbv3_op(k=3, s=1, ef=4, n=96),
-        mbv3_op(k=5, s=2, ef=8, n=160),
-        mbv3_op(k=5, s=1, ef=4, n=160),
-        mbv3_op(k=5, s=1, ef=4, n=160),
-        mbv3_op(k=5, s=1, ef=4, n=160),
-        mbv3_op(k=3, s=1, ef=8, n=192),
-        op(slim.conv2d, stride=1, num_outputs=1280, kernel_size=(1, 1)),
-    ])
 
 
 @slim.add_arg_scope
@@ -358,19 +249,9 @@ def wrapped_partial(func, new_defaults=None,
     return partial_func
 
 
-large = wrapped_partial(mobilenet, conv_defs=V3_LARGE)
-small = wrapped_partial(mobilenet, conv_defs=V3_SMALL)
-edge_tpu = wrapped_partial(mobilenet,
-                           new_defaults={'scope': 'MobilenetEdgeTPU'},
-                           conv_defs=V3_EDGETPU)
-
-# Minimalistic model that does not have Squeeze Excite blocks,
-# Hardswish, or 5x5 depthwise convolution.
-# This makes the model very friendly for a wide range of hardware
-large_minimalistic = wrapped_partial(
-    mobilenet, conv_defs=V3_LARGE_MINIMALISTIC)
-small_minimalistic = wrapped_partial(
-    mobilenet, conv_defs=V3_SMALL_MINIMALISTIC)
+# Allow Hard Swish in Large Minimalistic
+large_allow_hard = wrapped_partial(
+    mobilenet, conv_defs=V3_LARGE_ALLOW_HARD_SWISH)
 
 
 def _reduce_consecutive_layers(conv_defs, start_id, end_id, multiplier=0.5):
